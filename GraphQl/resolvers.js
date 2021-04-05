88c7128c66b70be81955049d829f89a4c8122eb7
scott012069 <scott012069@gmail.com>
2021-04-05 20:33:10 +0900

    1

---
+++ GraphQl/resolvers.js
@@ -0,0 +1,15 @@
+import fetch from "node-fetch";
+
+const API_URL = "https://yts.am/api/v2/list_movies.json";
+
+const getMovies = (limit, rating) =>
+  fetch(`${API_URL}`)
+    .then((res) => res.json())
+    .then((json) => json.data.movies);
+const resolvers = {
+  Query: {
+    movies: () => getMovies(),
+  },
+};
+
+export default resolvers;
